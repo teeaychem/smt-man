dfcfc637a663ec9a64aed565c95a9ae339543001
teeaychem <ben@sparkes.uk.net>
2025-10-26 18:45:37 +0000

    Ghost texture files
    No longer used, but may revisit before removal...

--- include/visual/textures.hpp
+++ include/visual/textures.hpp
@@ -1,31 +1,31 @@
-#pragma once
+// #pragma once
 
-#include "utils.hpp"
-#include <SDL3/SDL.h>
-#include <SDL3/SDL_main.h>
-#include <SDL3/SDL_render.h>
+// #include "utils.hpp"
+// #include <SDL3/SDL.h>
+// #include <SDL3/SDL_main.h>
+// #include <SDL3/SDL_render.h>
 
-#include <SDL3_image/SDL_image.h>
+// #include <SDL3_image/SDL_image.h>
 
-class AnimaTexture {
-public:
-  static constexpr float kOriginalSize = -1.f;
+// class AnimaTexture {
+// public:
+//   static constexpr float kOriginalSize = -1.f;
 
-  AnimaTexture();
+//   AnimaTexture();
 
-  ~AnimaTexture();
+//   ~AnimaTexture();
 
-  void destroy();
+//   void destroy();
 
-  void setColor(Uint8 r, Uint8 g, Uint8 b);
+//   void setColor(Uint8 r, Uint8 g, Uint8 b);
 
-  void setAlpha(Uint8 alpha);
+//   void setAlpha(Uint8 alpha);
 
-  Size getSize() const;
-  bool isLoaded() const;
+//   Size getSize() const;
+//   bool isLoaded() const;
 
-private:
-  SDL_Texture *aTexture;
+// private:
+//   SDL_Texture *aTexture;
 
-  Size size;
-};
+//   Size size;
+// };
--- src/visual/textures.cpp
+++ src/visual/textures.cpp
@@ -1,66 +1,66 @@
-#include "visual/textures.hpp"
-#include "unethical.hpp"
+// #include "visual/textures.hpp"
+// #include "unethical.hpp"
 
-AnimaTexture::AnimaTexture() : aTexture{nullptr},
-                               size{Size{kTileSize, kTileSize}} {
-}
-
-AnimaTexture::~AnimaTexture() {
-  destroy();
-}
-
-// bool AnimaTexture::mkRectangle(SDL_Renderer *renderer, int w, int h) {
-//   destroy();
-
-//   SDL_Surface *gSurface = SDL_CreateSurface(w, h, SDL_PIXELFORMAT_RGBA8888);
-
-//   if (gSurface == nullptr) {
-//     SDL_Log("SDL error: %s\n", SDL_GetError());
-//     return false;
-//   }
-
-//   bool surfaceKeyOk = SDL_SetSurfaceColorKey(gSurface, true, SDL_MapSurfaceRGB(gSurface, 0xFF, 0xFF, 0xFF));
-
-//   if (!surfaceKeyOk) {
-//     SDL_Log("SDL error: %s\n", SDL_GetError());
-//     return false;
-//   }
-
-//   SDL_FillSurfaceRect(gSurface, nullptr, SDL_MapSurfaceRGBA(gSurface, 50, 50, 50, 128));
-
-//   aTexture = SDL_CreateTextureFromSurface(renderer, gSurface);
-
-//   if (aTexture == nullptr) {
-//     SDL_Log("SDL error: %s\n", SDL_GetError());
-//     return false;
-//   }
-
-//   this->size.w = gSurface->w;
-//   this->size.h = gSurface->h;
-
-//   SDL_DestroySurface(gSurface);
-
-//   return aTexture != nullptr;
+// AnimaTexture::AnimaTexture() : aTexture{nullptr},
+//                                size{Size{kTileSize, kTileSize}} {
 // }
 
-void AnimaTexture::destroy() {
-  SDL_DestroyTexture(aTexture);
-  aTexture = nullptr;
-  size.scale(0);
-}
+// AnimaTexture::~AnimaTexture() {
+//   destroy();
+// }
 
-void AnimaTexture::setColor(Uint8 r, Uint8 g, Uint8 b) {
-  SDL_SetTextureColorMod(aTexture, r, g, b);
-}
+// // bool AnimaTexture::mkRectangle(SDL_Renderer *renderer, int w, int h) {
+// //   destroy();
 
-void AnimaTexture::setAlpha(Uint8 alpha) {
-  SDL_SetTextureAlphaMod(aTexture, alpha);
-}
+// //   SDL_Surface *gSurface = SDL_CreateSurface(w, h, SDL_PIXELFORMAT_RGBA8888);
 
-Size AnimaTexture::getSize() const {
-  return this->size;
-}
+// //   if (gSurface == nullptr) {
+// //     SDL_Log("SDL error: %s\n", SDL_GetError());
+// //     return false;
+// //   }
 
-bool AnimaTexture::isLoaded() const {
-  return aTexture != nullptr;
-}
+// //   bool surfaceKeyOk = SDL_SetSurfaceColorKey(gSurface, true, SDL_MapSurfaceRGB(gSurface, 0xFF, 0xFF, 0xFF));
+
+// //   if (!surfaceKeyOk) {
+// //     SDL_Log("SDL error: %s\n", SDL_GetError());
+// //     return false;
+// //   }
+
+// //   SDL_FillSurfaceRect(gSurface, nullptr, SDL_MapSurfaceRGBA(gSurface, 50, 50, 50, 128));
+
+// //   aTexture = SDL_CreateTextureFromSurface(renderer, gSurface);
+
+// //   if (aTexture == nullptr) {
+// //     SDL_Log("SDL error: %s\n", SDL_GetError());
+// //     return false;
+// //   }
+
+// //   this->size.w = gSurface->w;
+// //   this->size.h = gSurface->h;
+
+// //   SDL_DestroySurface(gSurface);
+
+// //   return aTexture != nullptr;
+// // }
+
+// void AnimaTexture::destroy() {
+//   SDL_DestroyTexture(aTexture);
+//   aTexture = nullptr;
+//   size.multiply(0);
+// }
+
+// void AnimaTexture::setColor(Uint8 r, Uint8 g, Uint8 b) {
+//   SDL_SetTextureColorMod(aTexture, r, g, b);
+// }
+
+// void AnimaTexture::setAlpha(Uint8 alpha) {
+//   SDL_SetTextureAlphaMod(aTexture, alpha);
+// }
+
+// Size AnimaTexture::getSize() const {
+//   return this->size;
+// }
+
+// bool AnimaTexture::isLoaded() const {
+//   return aTexture != nullptr;
+// }
