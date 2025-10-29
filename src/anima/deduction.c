4c5fa7e29f441990bef0a23414f8ad3068837325
teeaychem <ben@sparkes.uk.net>
2025-10-29 20:51:57 +0000

    Logic abstraction
    The idea is to have a solver for each anima.
    And, a term manager can be shared between solvers.
    As each solver will represent, mostly, of the same thing, the idea is to create a unique term manager and store shared terms (and sorts, etc.) in a library struct.
    
    This commit sketches the above and adds the sorts defined so far to the library.
    
    Also, the deduction.c file is simplified further.

--- src/anima/deduction.c
+++ src/anima/deduction.c
@@ -1,60 +1,33 @@
 
-#include "cvc5/c/cvc5.h"
+#include "logic.h"
 #include <stdio.h>
 
 void Anima_deduction_scratch() {
-  Cvc5TermManager *tm = cvc5_term_manager_new();
-  Cvc5 *slv = cvc5_new(tm);
+
+  Cvc5 *slv = cvc5_new(l_tm);
 
   cvc5_set_option(slv, "produce-models", "true");
   cvc5_set_option(slv, "finite-model-find", "true");
 
-  Cvc5Sort animaSort = cvc5_mk_uninterpreted_sort(tm, "Anima");
+  Cvc5Term facingFn = cvc5_mk_const(l_tm, llang.sorts.is_anima_facing, "Facing");
+  Cvc5Term blinky = cvc5_mk_const(l_tm, llang.sorts.anima, "blinky");
 
-  Cvc5Sort anima_fun_sorts[1] = {animaSort};
-  Cvc5Sort animaPredicate = cvc5_mk_fun_sort(tm, 1, anima_fun_sorts, cvc5_get_boolean_sort(tm));
-  Cvc5Sort directionSort = cvc5_mk_uninterpreted_sort(tm, "Direction");
+  Cvc5Term up = cvc5_mk_const(l_tm, llang.sorts.direction, "up");
+  Cvc5Term right = cvc5_mk_const(l_tm, llang.sorts.direction, "right");
+  Cvc5Term down = cvc5_mk_const(l_tm, llang.sorts.direction, "down");
+  Cvc5Term left = cvc5_mk_const(l_tm, llang.sorts.direction, "left");
 
-  Cvc5Sort fun_sorts[2] = {animaSort, directionSort};
-  Cvc5Sort facingSort = cvc5_mk_fun_sort(tm, 2, fun_sorts, cvc5_get_boolean_sort(tm));
+  Cvc5Term bu = cvc5_mk_term(l_tm, CVC5_KIND_APPLY_UF, 3, (Cvc5Term[3]){facingFn, blinky, up});
 
-  Cvc5Term facingFn = cvc5_mk_const(tm, facingSort, "Facing");
-  Cvc5Term blinky = cvc5_mk_const(tm, animaSort, "blinky");
+  Cvc5Term br = cvc5_mk_term(l_tm, CVC5_KIND_APPLY_UF, 3, (Cvc5Term[3]){facingFn, blinky, right});
 
-  Cvc5Term up = cvc5_mk_const(tm, directionSort, "up");
-  Cvc5Term right = cvc5_mk_const(tm, directionSort, "right");
-  Cvc5Term down = cvc5_mk_const(tm, directionSort, "down");
-  Cvc5Term left = cvc5_mk_const(tm, directionSort, "left");
+  Cvc5Term bd = cvc5_mk_term(l_tm, CVC5_KIND_APPLY_UF, 3, (Cvc5Term[3]){facingFn, blinky, down});
 
-  Cvc5Term direction_children[3] = {facingFn, blinky, up};
-  Cvc5Term bu = cvc5_mk_term(tm, CVC5_KIND_APPLY_UF, 3, direction_children);
+  Cvc5Term bl = cvc5_mk_term(l_tm, CVC5_KIND_APPLY_UF, 3, (Cvc5Term[3]){facingFn, blinky, left});
 
-  direction_children[2] = right;
-  Cvc5Term br = cvc5_mk_term(tm, CVC5_KIND_APPLY_UF, 3, direction_children);
+  Cvc5Term some_direction = cvc5_mk_term(l_tm, CVC5_KIND_XOR, 4, (Cvc5Term[4]){bu, br, bd, bl});
 
-  direction_children[2] = down;
-  Cvc5Term bd = cvc5_mk_term(tm, CVC5_KIND_APPLY_UF, 3, direction_children);
-
-  direction_children[2] = left;
-  Cvc5Term bl = cvc5_mk_term(tm, CVC5_KIND_APPLY_UF, 3, direction_children);
-
-  Cvc5Term burd_children[3] = {bu, br, bd};
-  Cvc5Term burd = cvc5_mk_term(tm, CVC5_KIND_OR, 3, burd_children);
-
-  Cvc5Term budl_children[3] = {bu, bd, bl};
-  Cvc5Term budl = cvc5_mk_term(tm, CVC5_KIND_OR, 3, budl_children);
-
-  Cvc5Term burl_children[3] = {bu, br, bl};
-  Cvc5Term burl = cvc5_mk_term(tm, CVC5_KIND_OR, 3, burl_children);
-
-  Cvc5Term brdl_children[3] = {br, bd, bl};
-  Cvc5Term brdl = cvc5_mk_term(tm, CVC5_KIND_OR, 3, brdl_children);
-
-  Cvc5Term some_direction_children[4] = {bu, br, bd, bl};
-  Cvc5Term some_direction = cvc5_mk_term(tm, CVC5_KIND_XOR, 4, some_direction_children);
-
-  Cvc5Term not_up_children[1] = {bu};
-  Cvc5Term not_up = cvc5_mk_term(tm, CVC5_KIND_NOT, 1, not_up_children);
+  Cvc5Term not_up = cvc5_mk_term(l_tm, CVC5_KIND_NOT, 1, (Cvc5Term[1]){bu});
 
   cvc5_assert_formula(slv, some_direction);
   cvc5_assert_formula(slv, not_up);
