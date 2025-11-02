b543afbd873a6a591f039d2e285c10f61d3eb07b
teeaychem <bsparkes@stanford.edu>
2025-11-02 16:16:19 +0000

    Unify cvc5 related anima things to a struct
    Syntactic, mostly.

--- src/anima/deduction.c
+++ src/anima/deduction.c
@@ -7,7 +7,7 @@
 void Anima_mind_innate(Anima *self) {
 
   cvc5_parser_set_str_input(
-      self->parser,
+      self->mind.parser,
       CVC5_LANG,
       "(declare-sort Anima 0)"
       "(declare-sort Direction 0)"
@@ -27,12 +27,12 @@ void Anima_mind_innate(Anima *self) {
       "(assert (forall ((anima Anima)) (xor (is_facing anima up) (xor (is_facing anima right) (xor (is_facing anima down) (is_facing anima left))))))",
       "anima_innate");
   do {
-    cvc5_cmd = cvc5_parser_next_command(self->parser, &cvc5_error_msg);
+    cvc5_cmd = cvc5_parser_next_command(self->mind.parser, &cvc5_error_msg);
     if (cvc5_error_msg) {
       printf("%s", cvc5_error_msg), exit(-1);
     }
     if (cvc5_cmd) {
-      cvc5_cmd_invoke(cvc5_cmd, self->mind, self->l_symbols);
+      cvc5_cmd_invoke(cvc5_cmd, self->mind.solver, self->mind.sm);
     }
   } while (cvc5_cmd);
 }
