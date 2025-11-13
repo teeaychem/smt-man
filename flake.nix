6df5f46bb7c76e410f7f24a6ccdac12c9a1bc62b
teeaychem <ben@sparkes.uk.net>
2025-11-13 19:46:41 +0000

    nixfmt on the flake

--- flake.nix
+++ flake.nix
@@ -1,48 +1,45 @@
 {
-    description = "smt-man flake";
+  description = "smt-man flake";
 
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
 
-    outputs = { self,
-                nixpkgs,
-                ...
-              }@inputs:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      ...
+    }@inputs:
 
     let
-    inherit (self) outputs;
+      inherit (self) outputs;
 
-    forAllSystems = nixpkgs.lib.genAttrs [
-      "x86_64-linux"
-      "aarch64-darwin"
-    ];
-  in {
-    devShells = forAllSystems (
-      system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        default = pkgs.mkShell {
-          nativeBuildInputs = [
-            pkgs.clang-tools
-            pkgs.clang
-            pkgs.cmake
-            pkgs.libpng
-            pkgs.sdl3
-            pkgs.pkg-config
-          ];
-          buildInputs = [
-            # Required by Criterion
-            pkgs.boxfort.dev
-            pkgs.libffi.dev
-            pkgs.libgit2.dev
-            pkgs.pcre2.dev
-            pkgs.nanomsg
-            pkgs.criterion.dev
-          ];
-        };
-      }
-    );
-  };
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            nativeBuildInputs = [
+              pkgs.clang-tools
+              pkgs.clang
+              pkgs.cmake
+              pkgs.criterion
+              pkgs.libpng
+              pkgs.sdl3
+              pkgs.pkg-config
+            ];
+            buildInputs = [ ];
+          };
+        }
+      );
+    };
 }
-
