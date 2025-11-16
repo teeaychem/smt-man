8286a052dd2339084e6c134bae279f85dce33a65
teeaychem <ben@sparkes.uk.net>
2025-11-16 23:14:20 +0000

    smaller steps
    datatypes and quantifiers did not interact as well as hoped
    so, back to a simple path using (this time) u8s

--- smt/explorations.py
+++ smt/explorations.py
@@ -20,6 +20,9 @@ with open("./resources/maze/source.txt", "r") as file:
 solver = Solver()
 
 
+bv8_0 = BitVecSort(8).cast(0)
+bv8_1 = BitVecSort(8).cast(1)
+
 bv8_u = z3.Const("u", BitVecSort(8))
 bv8_v = z3.Const("v", BitVecSort(8))
 bv8_w = z3.Const("w", BitVecSort(8))
@@ -33,7 +36,11 @@ maze_pairs = [[None for _ in range(0, width)] for _ in range(0, height)]
 z3dt_u8_pair = z3.Datatype("u8_pair_t")
 z3dt_u8_pair.declare("u8Pair", ("row", BitVecSort(8)), ("col", BitVecSort(8)))
 z3u8Pair = z3dt_u8_pair.create()
-v_u8p_rc = z3u8Pair.u8Pair(bv8_x, bv8_y)
+v_u8p_xy = z3u8Pair.u8Pair(bv8_x, bv8_y)
+v_u8p_uv = z3u8Pair.u8Pair(bv8_u, bv8_v)
+v_u8p_cxy = z3.Const("xy", z3u8Pair)
+v_u8p_cuv = z3.Const("uv", z3u8Pair)
+
 
 for r in range(0, height):
     for c in range(0, width):
@@ -76,24 +83,68 @@ solver.add(
     z3.ForAll([anima_anima], ULT(z3u8Pair.col(z3f_anima_location(anima_anima)), width))
 )
 
-for r in range(0, height):
-    for c in range(0, width):
-        solver.add(z3f_anima_location(anima_gottlob) != maze_pairs[r][c])
+# for r in range(0, height):
+#     for c in range(0, width):
+#         solver.add(z3f_anima_location(anima_gottlob) != maze_pairs[r][c])
 
 solver.assert_exprs(
     z3.Distinct(direction_up, direction_right, direction_down, direction_left)
 )
 
+solver.add(z3f_anima_location(anima_gottlob) == maze_pairs[1][2])
+solver.add(z3f_anima_location(anima_smtman) == maze_pairs[10][1])
 
-x = z3u8Pair.u8Pair(128, 0)
-print(x)
-print(f"{x} = ({simplify(z3u8Pair.row(x))}, {simplify(z3u8Pair.col(x))})")
+z3f_path = Function("path", BitVecSort(8), BitVecSort(8), BoolSort())
 
+
+solver.add(
+    z3.ForAll(
+        [anima_anima],
+        z3f_path(
+            z3u8Pair.row(z3f_anima_location(anima_smtman)),
+            z3u8Pair.col(z3f_anima_location(anima_smtman)),
+        ),
+    )
+)
+
+
+solver.add(
+    z3.ForAll(
+        [bv8_x, bv8_y],
+        z3.Implies(
+            z3f_path(bv8_x, bv8_y),
+            z3.Or(
+                z3f_path(bv8_x + bv8_1, bv8_y),
+                z3f_path(bv8_x - bv8_1, bv8_y),
+                z3f_path(bv8_x, bv8_y + bv8_1),
+                z3f_path(bv8_x, bv8_y - bv8_1),
+            ),
+        ),
+    )
+)
+
+solver.add(z3.Not(z3f_path(bv8_1, bv8_1)))
+
+
+# x = z3u8Pair.u8Pair(128, 0)
+# print(x)
+# print(f"{x} = ({simplify(z3u8Pair.row(x))}, {simplify(z3u8Pair.col(x))})")
+
+# input("Awaiting on input to solve...")
 result = solver.check()
 print(f"Result: {result}")
 if result == sat:
     model = solver.model()
     # print(f"{model}")
     print(model.eval(z3f_anima_facing(anima_gottlob)))
-    print(model.eval(z3f_anima_location(anima_gottlob)))
-    print(model.eval(z3u8Pair.row(z3f_anima_location(anima_gottlob))))
+    # print(model.eval(z3f_anima_location(anima_gottlob)))
+    # print(model.eval(z3u8Pair.row(z3f_anima_location(anima_gottlob))))
+    for r in range(0, height):
+        for c in range(0, width):
+            if model.eval(
+                z3f_path(
+                    z3u8Pair.row(maze_pairs[r][c]),
+                    z3u8Pair.col(maze_pairs[r][c]),
+                )
+            ):
+                print(f"({r}, {c})")
