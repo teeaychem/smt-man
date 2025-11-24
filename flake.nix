ecbbe1628cddc23927e599475f824ba8000d0798
teeaychem <ben@sparkes.uk.net>
2025-11-24 22:14:57 +0000

    revise the flake (again)
    https://fzakaria.com/2025/08/17/using-nix-as-a-library as inspiration
    and, a very informative disucssion on a closed tab (x_x) regardng buildInputs
    
    I'm not clear on the reasoning, but including pkg-config allows finding of flake dependent things when in a nix develop env, etc.

--- flake.nix
+++ flake.nix
@@ -1,59 +1,47 @@
 {
   description = "smt-man flake";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
   };
-
   outputs =
     {
       self,
       nixpkgs,
-      ...
-    }@inputs:
-
+    }:
     let
-      inherit (self) outputs;
-
-      forAllSystems = nixpkgs.lib.genAttrs [
+      lib = nixpkgs.lib;
+      systems = [
         "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
         "aarch64-darwin"
       ];
     in
     {
-      devShells = forAllSystems (
+      devShells = lib.genAttrs systems (
         system:
         let
-          pkgs = nixpkgs.legacyPackages.${system};
+          pkgs = import nixpkgs {
+            inherit system;
+          };
         in
         {
           default = pkgs.mkShell {
-            packages = [
-              pkgs.clang-tools
-              pkgs.clang
-              pkgs.cmake
+            buildInputs = [
               pkgs.criterion
               pkgs.cvc5
               pkgs.doxygen
               pkgs.libpng
               pkgs.sdl3
-              pkgs.pkg-config
               pkgs.z3
             ];
-          };
 
-          gcc = pkgs.mkShell.override { stdenv = pkgs.gccStdenv; } {
             packages = [
+              pkgs.clang
+
               pkgs.clang-tools
               pkgs.cmake
-              pkgs.criterion
-              pkgs.cvc5
-              pkgs.gcc
-              pkgs.doxygen
-              pkgs.libpng
-              pkgs.sdl3
               pkgs.pkg-config
-              pkgs.z3
             ];
           };
         }
