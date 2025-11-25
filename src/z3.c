8aa2d6f80e6a9f6344f9ad9037ef60b0a10957c6
teeaychem <ben@sparkes.uk.net>
2025-11-25 22:30:24 +0000

    Some progress
    An adapted version of tuple_example1 from the api test, as scratch.
    
    Failing to bump the reference count leads to a SIGSEGV.
    So, it looks as though the source of confusion has been resolved.
    Hek!
    Not sure how I missed the use.
    Well, time to move on.

--- src/z3.c
+++ src/z3.c
@@ -1,95 +1,121 @@
-#include "misc.h"
-#include "z3_api.h"
-#include "z3_optimization.h"
-#include <stdio.h>
 
-Z3_ast mk_binary_app(Z3_context ctx, Z3_func_decl f, Z3_ast x, Z3_ast y) {
-  return Z3_mk_app(ctx, f, 2, (Z3_ast[2]){x, y});
-}
+#include "z3.h"
+#include "misc.h"
+
+#include "z3.h"
+#include "z3_api.h"
+
+#include "clog.h"
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 void error_handler(Z3_context c, Z3_error_code e) {
-  printf("Error code: %d\n", e);
-  exit(-3);
+  ERROR("Z3 Error code: %d\n", e);
+  exit(1);
 }
 
-void z3_tmp(Maze *maze) {
+Z3_ast mk_var(Z3_context ctx, const char *name, Z3_sort ty) {
+  Z3_symbol symbol = Z3_mk_string_symbol(ctx, name);
+  return Z3_mk_const(ctx, symbol, ty);
+}
 
-  int32_t width_less_one = maze->size.x - 1;
-  int32_t height_less_one = maze->size.y - 1;
+Z3_ast mk_unary_app(Z3_context ctx, Z3_func_decl f, Z3_ast x) {
+  Z3_ast args[1] = {x};
+  return Z3_mk_app(ctx, f, 1, args);
+}
 
+Z3_ast mk_binary_app(Z3_context ctx, Z3_func_decl f, Z3_ast x, Z3_ast y) {
+  Z3_ast args[2] = {x, y};
+  return Z3_mk_app(ctx, f, 2, args);
+}
+
+void prove(Z3_context ctx, Z3_solver solver, Z3_ast statement) {
+  Z3_model model = 0;
+
+  Z3_solver_push(ctx, solver);
+
+  Z3_solver_assert(ctx, solver, Z3_mk_not(ctx, statement));
+
+  switch (Z3_solver_check(ctx, solver)) {
+  case Z3_L_FALSE: {
+    printf("VALID\n");
+  } break;
+  case Z3_L_UNDEF: {
+    printf("UNKNOWN\n");
+    model = Z3_solver_get_model(ctx, solver);
+    if (model != 0) {
+      Z3_model_inc_ref(ctx, model);
+      INFO("\nPotential counterexample:\n%s\n", Z3_model_to_string(ctx, model));
+    }
+  } break;
+  case Z3_L_TRUE: {
+    printf("INVALID\n");
+    model = Z3_solver_get_model(ctx, solver);
+    if (model) {
+      Z3_model_inc_ref(ctx, model);
+      INFO("\nCounterexample:\n%s\n", Z3_model_to_string(ctx, model));
+    }
+  } break;
+  }
+  if (model) {
+    Z3_model_dec_ref(ctx, model);
+  }
+
+  Z3_solver_pop(ctx, solver, 1);
+}
+
+void z3_tmp() {
   Z3_config cfg = Z3_mk_config();
   Z3_set_param_value(cfg, "model", "true");
 
   Z3_context ctx = Z3_mk_context(cfg);
+  Z3_set_error_handler(ctx, error_handler);
 
   Z3_del_config(cfg);
 
-  Z3_set_error_handler(ctx, error_handler);
-
-  Z3_solver solver = Z3_mk_solver(ctx);
+  Z3_solver s = Z3_mk_solver(ctx);
+  Z3_solver_inc_ref(ctx, s);
 
   Z3_sort u8_sort = Z3_mk_bv_sort(ctx, 8);
-  Z3_ast u8_zero = Z3_mk_numeral(ctx, "0", u8_sort);
-  Z3_ast u8_one = Z3_mk_numeral(ctx, "1", u8_sort);
 
-  unsigned int u8_sort_id = Z3_get_sort_id(ctx, u8_sort);
-  printf("u8_sort_id: %u\n", u8_sort_id);
-  printf("...\n");
-  printf("u8_sort is of bv size: %u\n", Z3_get_bv_sort_size(ctx, u8_sort));
+  Z3_symbol mk_tuple_name = Z3_mk_string_symbol(ctx, "mk_pair");
 
-  Z3_symbol u8p_accessor_names[2] = {Z3_mk_string_symbol(ctx, "fst"), Z3_mk_string_symbol(ctx, "snd")};
-  Z3_sort u8p_accessor_sorts[2] = {u8_sort, u8_sort};
-  unsigned u8p_accessor_sort_refs[2] = {0, 0};
+  Z3_symbol proj_names[2] = {Z3_mk_string_symbol(ctx, "get_x"),
+                             Z3_mk_string_symbol(ctx, "get_y")};
 
-  Z3_constructor u8p_con;
-  printf("c: %p", u8p_con);
+  Z3_sort proj_sorts[2] = {u8_sort, u8_sort};
 
-  u8p_con = Z3_mk_constructor(ctx,
-                              Z3_mk_string_symbol(ctx, "pair"),
-                              Z3_mk_string_symbol(ctx, "is_pair"),
-                              2,
-                              u8p_accessor_names,
-                              u8p_accessor_sorts,
-                              u8p_accessor_sort_refs);
+  Z3_func_decl mk_tuple_decl;
+  Z3_func_decl proj_decls[2];
+  Z3_sort pair_sort = Z3_mk_tuple_sort(ctx,
+                                       mk_tuple_name,
+                                       2,
+                                       proj_names,
+                                       proj_sorts,
+                                       &mk_tuple_decl,
+                                       proj_decls);
 
-  printf("c: %u", Z3_constructor_num_fields(ctx, u8p_con));
+  Z3_func_decl get_x_decl = proj_decls[0]; // get_x_decl(mk_pair(x,y)) = x
+  Z3_func_decl get_y_decl = proj_decls[1]; // get_y_decl(mk_pair(x,y)) = y
 
-  Z3_constructor constructors[1];
-  constructors[0] = u8p_con;
+  Z3_ast a = mk_var(ctx, "x", u8_sort);
+  Z3_ast b = mk_var(ctx, "y", u8_sort);
+  Z3_ast u8p_xy = mk_binary_app(ctx, mk_tuple_decl, a, b);
 
-  Z3_sort u8p_dt = Z3_mk_datatype(ctx, Z3_mk_string_symbol(ctx, "u8p_datatype"), 1, constructors);
+  Z3_ast one = Z3_mk_numeral(ctx, "1", u8_sort);
+  Z3_ast two = Z3_mk_numeral(ctx, "2", u8_sort);
+  Z3_ast three = Z3_mk_numeral(ctx, "3", u8_sort);
 
-  Z3_func_decl u8p_decl;
-  Z3_func_decl is_u8p_decl;
-  Z3_func_decl u8p_accessors[2];
-  Z3_query_constructor(ctx, u8p_con, 2, &u8p_decl, &is_u8p_decl, u8p_accessors);
-  Z3_del_constructor(ctx, u8p_con);
+  Z3_ast get_x = mk_unary_app(ctx, get_x_decl, u8p_xy);
+  Z3_ast get_y = mk_unary_app(ctx, get_y_decl, u8p_xy);
 
-  /* Z3_symbol s = Z3_mk_string_symbol(ctx, "zo"); */
-  /* Z3_ast zo = Z3_mk_const(ctx, s, u8p_dt); */
+  prove(ctx, s, Z3_mk_eq(ctx, Z3_mk_bvor(ctx, get_x, get_y), three));
 
-  /* Z3_ast p1 = mk_binary_app(ctx, u8p_decl, u8_zero, u8_zero); */
-  /* Z3_ast p2 = mk_binary_app(ctx, u8p_decl, u8_zero, u8_zero); */
+  Z3_ast u8p_onetwo = mk_binary_app(ctx, mk_tuple_decl, one, two);
+  prove(ctx, s, Z3_mk_eq(ctx, Z3_mk_bvand(ctx, mk_unary_app(ctx, get_x_decl, u8p_onetwo), mk_unary_app(ctx, get_y_decl, u8p_onetwo)), three));
 
-  Z3_ast eqp = Z3_mk_eq(ctx, u8_zero, u8_zero);
-
-  /* Z3_solver_push(ctx, solver); */
-  Z3_solver_assert(ctx, solver, eqp);
-
-  auto result = Z3_solver_check(ctx, solver);
-
-  switch (result) {
-  case Z3_L_FALSE: {
-    printf("UNSAT");
-  } break;
-  case Z3_L_UNDEF: {
-    printf("UNDEF");
-  } break;
-  case Z3_L_TRUE: {
-    printf("SAT");
-  } break;
-  }
-  printf("\n");
-
-  /* Z3_solver_pop(ctx, solver, 1); */
+  Z3_ast u8p_onethree = mk_binary_app(ctx, mk_tuple_decl, one, three);
+  prove(ctx, s, Z3_mk_eq(ctx, Z3_mk_bvand(ctx, mk_unary_app(ctx, get_x_decl, u8p_onethree), mk_unary_app(ctx, get_y_decl, u8p_onethree)), one));
 }
