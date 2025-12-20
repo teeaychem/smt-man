a30b355cf0d0f5c0089ae196d35832587e6aab2d
teeaychem <ben@sparkes.uk.net>
2025-12-20 15:12:46 +0000

    Maze_detail_arc_inner: Initial pass at inner arcs
    For the moment only a handful of simple cases.

--- src/core/maze.c
+++ src/core/maze.c
@@ -373,25 +373,36 @@ void Maze_detail_arc_outer(Maze *self) {
   }
 }
 
-/* void Maze_detail_arc_inner(Maze *self) { */
-/*   for (uint8_t c = 0; c < self->size.y; ++c) { */
-/*     for (uint8_t r = 0; r < self->size.x; ++r) { */
-/*       TileData *tile = Maze_abstract_at(self, c, r); */
-/*       if (tile->type == TILE_EDGE) { */
-/*         if (c == 0 && r == 0) { */
-/*           tile->value.edge_value.edge_style = TILE_STYLE_ARC; */
-/*           tile->value.edge_value.edge_arc_value = FIRST; */
-/*         } */
+void Maze_detail_arc_inner(Maze *self) {
+  for (uint8_t col = 1; col < self->size.x - 1; ++col) {
+    for (uint8_t row = 3; row < self->size.y - 3; ++row) {
+      TileData *tile = Maze_abstract_at(self, col, row);
+      if (tile->type == TILE_EDGE) {
+        if ((Maze_abstract_at(self, col + 1, row)->type != TILE_EDGE) &&
+            (Maze_abstract_at(self, col, row - 1)->type != TILE_EDGE)) {
+          Tile_set_arc(tile, FIRST);
+        }
 
-/*         if (c == 0 && r + 1 == self->size.y) { */
-/*           tile->value.edge_value.edge_style = TILE_STYLE_ARC; */
-/*           tile->value.edge_value.edge_arc_value = THIRD; */
-/*         } */
-/*       } */
-/*     } */
-/*   } */
-/* } */
+        if ((Maze_abstract_at(self, col - 1, row)->type != TILE_EDGE) &&
+            (Maze_abstract_at(self, col, row - 1)->type != TILE_EDGE)) {
+          Tile_set_arc(tile, SECOND);
+        }
+
+        if ((Maze_abstract_at(self, col - 1, row)->type != TILE_EDGE) &&
+            (Maze_abstract_at(self, col, row + 1)->type != TILE_EDGE)) {
+          Tile_set_arc(tile, THIRD);
+        }
+
+        if ((Maze_abstract_at(self, col + 1, row)->type != TILE_EDGE) &&
+            (Maze_abstract_at(self, col, row + 1)->type != TILE_EDGE)) {
+          Tile_set_arc(tile, FOURTH);
+        }
+      }
+    }
+  }
+}
 
 void Maze_detail(Maze *self) {
   Maze_detail_arc_outer(self);
+  Maze_detail_arc_inner(self);
 }
