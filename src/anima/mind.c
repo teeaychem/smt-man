cd44679eaa1b27b3ccbf547d5586c312389955ca
teeaychem <ben@sparkes.uk.net>
2026-01-03 11:25:35 +0000

    Chase
    With a little reflection, path information is sufficient to guide an anima.
    For, given a solve path at the location of the anima at the time of the solve with be an orgin and some direction.
    And, whatever the direction is, that's the direction the anima should move in.
    No significant deduction required.
    
    This works, and is almost a genuine chase.
    
    A clear issue, though, is latency.
    This was known, but it's *very* easy for an anima to miss a turning and then be in a loop of turning around, missing, turning around, and so on.
    
    A solution to this is to only allow changes at intersections.
    It'd also be interesting to see how this affects solve times.
    
    Assuming solve times aren't sub-frame, there may still be issues when an anima is on the tail of the persona.
    If these are an issue, it might be useful to set some distance within which 'instinct' takes over and pathing is handed imperitavely.

--- src/anima/mind.c
+++ src/anima/mind.c
@@ -1,4 +1,5 @@
 #include <stdatomic.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "logic.h"
@@ -48,6 +49,8 @@ void Mind_deduct(Mind *self, const Maze *maze) {
 
   Z3_optimize_push(self->ctx, self->opz);
 
+  auto anima_location = atomic_load(&self->view.anima[self->id].location);
+
   Lang_assert_anima_location(&self->lang, self->ctx, self->opz, &self->view, self->id);
   Lang_assert_persona_location(&self->lang, self->ctx, self->opz, &self->view);
   Lang_assert_link_reqs(&self->lang, self->ctx, self->opz, &self->view, maze, self->id);
@@ -73,26 +76,51 @@ void Mind_deduct(Mind *self, const Maze *maze) {
   Z3_model model = Z3_optimize_get_model(self->ctx, self->opz);
   Z3_model_inc_ref(self->ctx, model);
 
-  /* Z3_ast anima_direction = nullptr; */
-  /* Z3_model_eval(self->ctx, model, Z3_mk_app(self->ctx, self->lang.anima.is_facing, 1, &anima_ast), false, &anima_direction); */
+  Z3_ast anima_origin = nullptr;
 
-  switch (random_in_range(1, 4)) {
-  case 1: {
+  Z3_ast row_col[2] = {
+      Z3_mk_int(self->ctx, anima_location.x, self->lang.u8.sort),
+      Z3_mk_int(self->ctx, anima_location.y, self->lang.u8.sort),
+  };
+  auto tile = Z3_mk_app(self->ctx, self->lang.path.tile_is_f, 2, row_col);
+  Z3_model_eval(self->ctx, model, tile, false, &anima_origin);
+
+  if (anima_origin == self->lang.path.og_up) {
     self->direction_intent = DIRECTION_N;
-  } break;
-  case 2: {
+  }
+
+  else if (anima_origin == self->lang.path.og_rt) {
     self->direction_intent = DIRECTION_E;
-  } break;
-  case 3: {
+  }
+
+  else if (anima_origin == self->lang.path.og_dn) {
     self->direction_intent = DIRECTION_S;
-  } break;
-  case 4: {
+  }
+
+  else if (anima_origin == self->lang.path.og_lt) {
     self->direction_intent = DIRECTION_W;
-  } break;
-  default: {
-    g_log(nullptr, G_LOG_LEVEL_WARNING, "No direction");
-    exit(-1);
-  } break;
+  }
+
+  else {
+    // Backup
+    switch (random_in_range(1, 4)) {
+    case 1: {
+      self->direction_intent = DIRECTION_N;
+    } break;
+    case 2: {
+      self->direction_intent = DIRECTION_E;
+    } break;
+    case 3: {
+      self->direction_intent = DIRECTION_S;
+    } break;
+    case 4: {
+      self->direction_intent = DIRECTION_W;
+    } break;
+    default: {
+      g_log(nullptr, G_LOG_LEVEL_WARNING, "No direction");
+      exit(-1);
+    } break;
+    }
   }
 
   Z3_model_dec_ref(self->ctx, model);
